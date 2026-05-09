f72f595bac50c05ccad0a6b892ff6e5c40e173a6
Solntseva Julia <solncevaulia0@gmail.com>
2026-05-09 18:02:28 +0300

    исправила Main

--- src/main/java/org/example/demo/Main.java
+++ src/main/java/org/example/demo/Main.java
@@ -2,6 +2,6 @@ package org.example.demo1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет");
+        System.out.println("Приветeirf");
     }
 }
